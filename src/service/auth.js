a19baf4d939d576038f1696fcc14c6ba1a919da5
asgabrielsilva <gabrielalmeida290905@gmail.com>
2024-10-31 09:25:05 -0300

    autorisação

--- src/service/auth.js
+++ src/service/auth.js
@@ -3,7 +3,11 @@ import axios from 'axios';
 export default class AuthService {
   async postUserToken(token) {
     console.log(token)
-    const response = await axios.get('/usuarios/me/');
+    const response = await axios.get("/usuarios/me/", {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
     return response.data;
   }
 }
